9f1c793ad1b411aa3c55044956bc41f944dba8e7
juansccp <juanlucio739@gmail.com>
2026-05-12 10:36:37 -0300

    Delete parah lanches directory

--- parah lanches/js/script.js
+++
@@ -1,9 +0,0 @@
-function VerificarLogin() {
-    let login = document.getElementById("login").value;
-    let senha = document.getElementById("senha").value;
-    if (login === "admin" && senha === "123456") {
-       alert("acesso liberado.");
-    } else {
-       alert("acesso negado.");
-    }
-}
