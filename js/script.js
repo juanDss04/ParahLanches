9b27c2f64ed46d402b1a4f4ea4b098a8356242c0
juansccp <juanlucio739@gmail.com>
2026-05-12 10:33:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function VerificarLogin() {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+    if (login === "admin" && senha === "123456") {
+       alert("acesso liberado.");
+    } else {
+       alert("acesso negado.");
+    }
+}
